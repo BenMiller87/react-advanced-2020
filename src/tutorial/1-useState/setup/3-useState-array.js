47c2bc15d14f5ddc982411baaea0d195032cf5d9
Ben <ben.miller@qr.com.au>
2022-03-17 13:38:23 +1000

    use state complete

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button
+              onClick={() => {
+                setPeople(people.filter((person) => person.id !== id));
+              }}
+              className="btn"
+            >
+              remove person
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default UseStateArray;
